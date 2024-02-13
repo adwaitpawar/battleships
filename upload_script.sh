3a88c3f59e4e16d853e26e465dbcd6970e3313e5
adwaitpawar <110279221+adwaitpawar@users.noreply.github.com>
2024-02-13 15:16:12 +0530

    Update upload_script.sh

--- upload_script.sh
+++ upload_script.sh
@@ -3,7 +3,7 @@
 REPOSITORY_URL="http://65.2.180.154:8081/repository/Battleship-repo/"  # Replace with your Nexus repository URL
 NEXUS_USERNAME="admin"  # Replace with Nexus repository username
 NEXUS_PASSWORD="nexus@123"  # Replace with Nexus repository password
-ZIP_FILE="battleships.zip"  # Path to the ZIP file to upload
+TGZ_FILE="battleships.tgz"  # Path to the ZIP file to upload
 
 # Authenticate with Nexus repository and upload ZIP file
-curl -u "$NEXUS_USERNAME:$NEXUS_PASSWORD" -X POST "$REPOSITORY_URL" -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "artifact=@$ZIP_FILE;type=application/zip"
+curl -u "$NEXUS_USERNAME:$NEXUS_PASSWORD" -X POST "$REPOSITORY_URL" -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "artifact=@$TGZ_FILE;type=application/gzip"
